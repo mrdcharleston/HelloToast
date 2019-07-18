e2dfa36b584e890d9ca367921892922500d4c5ad
mrdcharleston <mrdcharleston@gmail.com>
2019-07-18 12:38:39 -0700

    Finished App From Android Fundamental 01.2

---
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.hellotoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button makeToastButton;
+    TextView counterTextView;
+    Button counterButton;
+    private int number;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        makeToastButton = findViewById(R.id.toast_button);
+        counterTextView = findViewById(R.id.counter);
+        counterButton = findViewById(R.id.counter_button);
+
+
+        makeToastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(),"Hello Toast!",Toast.LENGTH_SHORT);
+                toast.show();
+
+            }
+        });
+
+        counterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                counterTextView.setText((String.valueOf(number ++)));
+
+
+            }
+        });
+
+    }
+
+
+
+
+}
